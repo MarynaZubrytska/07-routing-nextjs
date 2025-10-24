e9e9f7806f26b63d482f7a179fa78eed324347de
MarynaZ <maryzubritskaya@gmail.com>
2025-10-25 01:35:34 +0300

    Fix loading

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import NotePreview from '@/components/NotePreview/NotePreview'
+import NotePreview from '@/app/@modal/NotePreview'
 
 export default function InterceptedNoteModal() {
   return <NotePreview />
